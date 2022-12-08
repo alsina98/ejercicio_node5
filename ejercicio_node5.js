04f67d4e07f5917207d8a8629aaa6b297e1ba23b
Jordi <91033826+alsina98@users.noreply.github.com>
2022-12-08 13:47:08 +0100

    Update ejercicio_node5.js

--- ejercicio_node5.js
+++ ejercicio_node5.js
@@ -1,18 +1,24 @@
+//EX1
 let altura_cm = 179 
 console.log(altura_cm);
 
+//EX2
 let altura_metros = 1.79
 console.log(altura_metros);
 
+//EX3
 let peso = 75.2
 console.log(peso);
 
+//EX4
 let altura_metros_arriba = 1.79
 console.log(Math.ceil(altura_metros_arriba));
 
+//EX5
 let peso_abajo = 75.2
 console.log(Math.floor(peso_abajo));
 
+//EX6
 var numero1 = 1.79E+308;
 var numero2 = 1.79E+309;
 let text;
