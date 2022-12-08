ce8140521afc5a5e75549497941f0783c444a983
Jordi <91033826+alsina98@users.noreply.github.com>
2022-12-08 13:46:18 +0100

    Create ejercicio_node5.js

---
+++ ejercicio_node5.js
@@ -0,0 +1,32 @@
+let altura_cm = 179 
+console.log(altura_cm);
+
+let altura_metros = 1.79
+console.log(altura_metros);
+
+let peso = 75.2
+console.log(peso);
+
+let altura_metros_arriba = 1.79
+console.log(Math.ceil(altura_metros_arriba));
+
+let peso_abajo = 75.2
+console.log(Math.floor(peso_abajo));
+
+var numero1 = 1.79E+308;
+var numero2 = 1.79E+309;
+let text;
+
+function verificarValorMaximo(num){
+    if (num <= Number.MAX_VALUE){
+        return text = "El numero no es infinito"
+    }else{
+        return text = "El numero es infinito"
+    }
+}
+
+console.log(verificarValorMaximo(numero1));
+console.log(verificarValorMaximo(numero2));
+
+console.log(numero1);
+console.log(numero2);
